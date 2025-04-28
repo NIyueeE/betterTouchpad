761185d3c005994c23958bb2fc361f5a6df1c8d2
NIyue <cuijy21@mails.tsinghua.edu.cn>
2025-04-28 11:32:53 +0800

    移除了缓存文件, 和exe打包的临时文件

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from .touchpad_controller import TouchpadController
-
-if __name__ == "__main__":
-    """
-    程序入口点
-    创建并运行触控板控制器
-    """
-    controller = TouchpadController()
-    controller.run()
